56c00f902e0ede0a7dad550fdc453a994771f244
likun <likun@iboxpay.com>
2015-07-06 21:16:09 +0800

    add sass compile

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,33 @@
 'use strict';
 
 var gulp = require('gulp');
+var util=require('util');
 var $ = require('gulp-load-plugins')();
 
 
 
 var filePath = {
-    index: 'app/index',
-    about: 'app/about'
+    index: 'src/app/index',
+    about: 'src/app/about'
 };
+
+var conf = {
+    tmp:'.tmp',
+    src:'src'
+};
+
+
+gulp.task('style',function(){
+    return gulp.src(conf.src+'/app/**/*.scss')
+        .pipe($.sass())
+        .pipe(gulp.dest(conf.tmp+'/css'));
+});
 
 
 gulp.task('inject', function() {
 
-  return gulp.src('app/*.html')
+
+    return gulp.src('src/app/*.html')
         .pipe($.inject(gulp.src(filePath.index + '/**/*.js'), {
             name: 'inject:index'
         }))
@@ -24,23 +38,82 @@ gulp.task('inject', function() {
 
 });
 
-gulp.task('build', ['inject'], function(){
 
-  //var assets = $.useref.assets();
-  var jsFilter = $.filter('**/*.js');
-  var cssFilter = $.filter('**/*.css');
+var browserSync = require('browser-sync');
 
-  var assets = $.useref.assets({searchPath: './'});
-  return gulp.src('.tmp/*.html')
-         .pipe(assets)
-         .pipe(jsFilter)
-         .pipe($.uglify())
-         .pipe(jsFilter.restore())
-         .pipe($.rev())
-         .pipe(assets.restore())
-         .pipe($.useref())
-         .pipe($.revReplace())
-         .pipe(gulp.dest('dist'));
+// var middleware = require('./proxy');
+
+function browserSyncInit(baseDir, files, isSpecRunner, browser) {
+    browser = browser === undefined ? 'default' : browser;
+
+    var routes = null;
+    if (baseDir === conf.src || (util.isArray(baseDir) && baseDir.indexOf(conf.src) !== -1)) {
+        routes = {
+            '/bower_components': 'bower_components',
+            '/node_modules': 'node_modules'
+        };
+    }
+
+    var startPath = '/';
+    if (isSpecRunner) {
+        startPath += conf.specRunnerFile;
+    }
+
+    browserSync.instance = browserSync.init(files, {
+        startPath: startPath,
+        server: {
+            baseDir: baseDir,
+            routes: routes
+        },
+        browser: browser,
+        ghostMode: {
+            clicks: true,
+            location: false,
+            forms: true,
+            scroll: true
+        },
+        injectChanges: true,
+        reloadDelay: 1000
+    });
+}
+
+
+gulp.task('watch',['inject','style'],function(){
+    gulp.watch('src/app/**/*.*',['inject']);
+    gulp.watch('src/app/**/*.scss',['style'])
+});
+
+
+
+gulp.task('serve', ['inject','watch'], function() {
+    browserSyncInit([
+        conf.tmp + '',
+        './'
+    ], []);
+});
+
+
+
+gulp.task('build', ['inject'], function() {
+
+    //var assets = $.useref.assets();
+    var jsFilter = $.filter('**/*.js');
+    var cssFilter = $.filter('**/*.css');
+
+    var assets = $.useref.assets({
+        searchPath: './'
+    });
+
+    return gulp.src('.tmp/*.html')
+        .pipe(assets)
+        .pipe(jsFilter)
+        .pipe($.uglify())
+        .pipe(jsFilter.restore())
+        .pipe($.rev())
+        .pipe(assets.restore())
+        .pipe($.useref())
+        .pipe($.revReplace())
+        .pipe(gulp.dest('dist'));
 });
 
 
